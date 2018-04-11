5a91a44731301b5084226e9413beaf9c2f1b9f55
Bianca Power <drbiancampower@gmail.com>
2018-04-11 10:00:45 +1000

    generate lollies scaffold

---
+++ db/migrate/20180410235858_create_lollies.rb
@@ -0,0 +1,11 @@
+class CreateLollies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :lollies do |t|
+      t.string :name
+      t.text :description
+      t.string :price
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/system/lollies_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class LolliesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit lollies_url
+  #
+  #   assert_selector "h1", text: "Lolly"
+  # end
+end
